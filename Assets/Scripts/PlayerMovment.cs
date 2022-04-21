650e53a5b0419eacdc37d6166fb8d7cf09bf1f9c
Ali Emre Deneri <ali.emre.deneri@gmail.com>
2022-04-21 17:58:33 +0300

    Added animations

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
     public CharacterController controller;
+    public Animator animator;
 
     public float speed = 12f;
     public float jumpHeight = 3f;
@@ -11,6 +12,9 @@ public class PlayerMovment : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundMask;
     private float groundDistance = 0.4f;
+
+    private bool isMoving;
+    private bool isRunning;
     
     
     private Vector3 velocity;
@@ -19,6 +23,7 @@ public class PlayerMovment : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
+        
     }
 
     // Update is called once per frame
@@ -40,18 +45,32 @@ public class PlayerMovment : MonoBehaviour
 
     private void Spirit() {
         speed = Input.GetKey(KeyCode.LeftShift) ? 20f : 12f;
+        if (speed > 13 && isMoving) {
+            animator.SetBool("isRunning",true);
+        }
+        else {
+            animator.SetBool("isRunning",false);
+        }
     }
 
     private void Jump() {
         if (Input.GetButtonDown("Jump") && isGrounded) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("isJumping",true);
         }
+        animator.SetBool("isJumping",false);
     }
 
     private void Move() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+        
+        //anim
+        isMoving = x != 0 || z != 0;
+        animator.SetBool("isWalking",isMoving);
+
     }
 }
